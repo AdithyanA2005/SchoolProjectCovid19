532cf557d2d82f3bfe4dd992339573e537be7436
Adithyan.A <deepaadithyan56@gmail.com>
2020-11-18 14:54:04 +0530

    Barchart Updated

--- assets/js/bar.js
+++ assets/js/bar.js
@@ -1,14 +1,14 @@
 new Chart(document.getElementById("barchart"), {
 	type: 'bar',
 	data: {
-		labels: ['May', 'June', 'July', 'August', 'September', 'October', 'November'],
+		labels: ['February','March','April', 'May', 'June', 'July', 'August', 'September', 'October'],
 		datasets: [{
-			data: [1269,4442,23613,75385,198106,433105,12345],
+			data: [7,241,497,1269,4442,23613,75385,198106,433105],
 			label: 'Kerala',
 			backgroundColor: "#4755AB",
 			borderWidth: 1,
 		}, {
-			data: [182,837,2512,5154,14457,20787,25456],
+			data: [0,57,113,182,837,2512,5154,14457,20787],
 			label: 'Himachal Pradesh',
 			backgroundColor: "#c500e3",
 			borderWidth: 1,
