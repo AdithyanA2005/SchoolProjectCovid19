41e563109f25ce3b3fecc150bac48c50855937a5
Adithyan A <deepaadithyan56@gmail.com>
2022-06-04 15:30:12 +0530

    Project organised

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-from covid_stats.main import app
-from covid_stats import views
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
